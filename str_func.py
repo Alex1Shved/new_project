d1a5b925aebbf97c94e4062c11299b58cd799062
Alex1Shved <124492815+Alex1Shved@users.noreply.github.com>
2023-02-18 13:53:34 +0300

    Update str_func.py
    Добавил функцию мазафака

--- str_func.py
+++ str_func.py
@@ -4,3 +4,7 @@ def str():
     return str
 a = str()
 print(a)
+def str_1():
+    '''Функция которая принимает на вход строку и возвращает её, с первыми заглавными буквами'''
+    str = input().title()
+    return str
